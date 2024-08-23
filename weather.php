07b0f72396518e4539275331afddfdb800016959
devansh-gupta03 <142433214+devansh-gupta03@users.noreply.github.com>
2024-08-23 15:46:57 +0530

    Update weather.php

--- weather.php
+++ weather.php
@@ -7,19 +7,19 @@ ini_set('display_errors', 1);
 
 
 
-// Get city name from query parameter
-$city = isset($_GET['city']) ? $_GET['city'] : 'London'; // Default to London if no city is provided
 
-$apiKey = "ec42b1c4fef77541974273973160b776"; // Replace with your API key
+$city = isset($_GET['city']) ? $_GET['city'] : 'London'; 
+
+$apiKey = "ec42b1c4fef77541974273973160b776"; 
 $apiUrl = "http://api.openweathermap.org/data/2.5/weather?q={$city}&appid={$apiKey}&units=metric";
 
-// Fetch data from the API
+
 $weatherData = file_get_contents($apiUrl);
 
-// Convert JSON data to PHP array
+
 $weatherArray = json_decode($weatherData, true);
 
-// Send the weather data back as JSON for JavaScript to use
+
 header('Content-Type: application/json');
 echo json_encode($weatherArray);
 
